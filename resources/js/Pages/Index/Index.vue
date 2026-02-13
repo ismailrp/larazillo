63196e3de647d71a3a80f476cafe879846632a1e
Ismail <ismail.rintoprasetio@gmail.com>
2026-02-13 08:38:55 +0700

    committt

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+	<div>Index</div>
+	<Link href="/show">Show</Link>
+  {{ message }}
+  </div>
+</template>
+
+<script setup>
+import {Link} from "@inertiajs/vue3"; 
+defineProps({
+  message: String
+});
+</script>
+
+
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+	<div>Show</div>
+	<Link href="/index">Index</Link>
+  </div>
+</template>
+<script setup>
+import {Link} from "@inertiajs/vue3"; 
+</script>
